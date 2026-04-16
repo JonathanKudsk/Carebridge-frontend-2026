fa5a50716b5cd817c09ceb835630d682428563fa
miaduedam <duedamm@gmail.com>
2026-04-16 21:51:54 +0200

    Add message page button to open new chat modal

--- src/pages/(worker)/MessagePage.jsx
+++ src/pages/(worker)/MessagePage.jsx
@@ -1,3 +1,15 @@
+import { useState } from "react";
+import { Button } from "react-bootstrap";
+import NewChatModal from "../../components/NewChatModal";
+
 export default function MessagePage() {
-  return <div>Beskedside</div>;
+  const [showModal, setShowModal] = useState(false);
+
+  return (
+    <div>
+      <Button onClick={() => setShowModal(true)}>Ny Chat</Button>
+
+      <NewChatModal show={showModal} onHide={() => setShowModal(false)} />
+    </div>
+  );
 }
