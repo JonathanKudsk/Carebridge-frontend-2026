85e224823143f7ba641bebcc160351956f7d9e85
istimereal <jjfapir@gmail.com>
2026-05-02 18:21:20 +0200

    File for eventMapper created in src/utils

---
+++ src/utils/eventMappers.js
@@ -0,0 +1,45 @@
+export const ACCESS_LEVEL_OPTIONS = [
+  {
+    value: "1",
+    level: 1,
+    label: "Faellesaktivitet / pleje",
+    riskColor: "green",
+    riskDescription: "Ingen risiko",
+  },
+  {
+    value: "2",
+    level: 2,
+    label: "Transport / praktisk",
+    riskColor: "yellow",
+    riskDescription: "Lav risiko",
+  },
+  {
+    value: "3",
+    level: 3,
+    label: "Sundhed / medicin",
+    riskColor: "orange",
+    riskDescription: "Moderat risiko",
+  },
+  {
+    value: "4",
+    level: 4,
+    label: "Myndigheder / psykolog",
+    riskColor: "red",
+    riskDescription: "Høj risiko",
+  },
+  {
+    value: "5",
+    level: 5,
+    label: "Akut / juridisk",
+    riskColor: "purple",
+    riskDescription: "Kritisk risiko",
+  },
+];
+
+export function getAccessLevelOption(accessLevel) {
+  return (
+    ACCESS_LEVEL_OPTIONS.find(
+      (option) => String(option.value) === String(accessLevel)
+    ) || ACCESS_LEVEL_OPTIONS[0]
+  );
+}
