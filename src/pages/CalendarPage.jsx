30886da2d8374dd8dc29259e46e3c1565fe800eb
istimereal <jjfapir@gmail.com>
2026-05-03 13:52:10 +0200

    Fix calendar create callback name

--- src/pages/CalendarPage.jsx
+++ src/pages/CalendarPage.jsx
@@ -226,7 +226,7 @@ export default function CalendarPage() {
       {!loading && (
         <ContinuousCalendar
           events={events}
-          onCreate={handleCreate}
+          onCreate={}
           onEventClick={(e) => console.log("clicked event", e)}
           onDelete={handleDelete}
         />
